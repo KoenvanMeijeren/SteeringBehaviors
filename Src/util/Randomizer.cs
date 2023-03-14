9eb5fdc8bdebe1e403f1f12c79811c289b4c4580
Job <s1158075@student.windesheim.nl>
2023-03-14 16:47:56 +0100

    Add randomizer

--- Src/util/Randomizer.cs
+++ Src/util/Randomizer.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Src.util
 {
-    internal class Randomizer
+    public static class Randomizer
     {
+        public static Random RandomizerObject = new Random();
+
+        public static int GetRandomNumber(int minimum, int maximum)
+        {
+            return RandomizerObject.Next(minimum, maximum);
+        }
+
+        public static double GetRandomNumber(double minimum, double maximum)
+        {
+            return RandomizerObject.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        public static int RandomClamped()
+        {
+            return RandomizerObject.NextDouble() >= 0.5 ? -1 : 1;
+        }
     }
 }
