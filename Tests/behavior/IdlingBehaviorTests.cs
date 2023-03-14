06efe2fb4a2ae310befc52782b6fbd5cff40d4a3
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-03-14 15:58:17 +0100

    Add tests for idling behavior

--- Tests/behavior/IdlingBehaviorTests.cs
+++ Tests/behavior/IdlingBehaviorTests.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using Src.behavior;
+using Src.entity;
 using Src.util;
+using Tests.behavior.util;
 using Tests.fixtures.world;
 
 namespace Tests.behavior
@@ -40,6 +42,91 @@ namespace Tests.behavior
 
             // Assert
             Assert.AreEqual(expectedResult, velocity.ToString());
+        }
+    }
+    
+    public class MovingEntityWithIdlingBehaviorTests
+    {
+        [Test]
+        public void TestWorldUpdate_01_Ok()
+        {
+            // Arrange
+            const float TimeElapsed = 0.800000012f;
+            Vector seekingEntityPosition = new Vector(35, 35);
+            Vector targetEntityPosition = new Vector(249, 249);
+            WorldTest world = new WorldTest(250, 250, seekingEntityPosition, targetEntityPosition);
+            ISteeringBehavior steeringBehavior = new IdlingBehavior(world.SeekingEntity);
+            IMovingEntity movingEntity = world.SeekingEntity;
+            movingEntity.SetSteeringBehavior(steeringBehavior);
+
+            // Act & assert
+            Assert.AreEqual("(35,35)", movingEntity.Position.ToString());
+            Assert.AreEqual("(0,0)", movingEntity.Velocity.ToString());
+
+            for (int index = 0; index < 100; index++)
+            {
+                world.Update(TimeElapsed);
+            }
+
+            Assert.AreEqual("(35,35)", movingEntity.Position.ToString());
+            Assert.AreEqual("(0,0)", movingEntity.Velocity.ToString());
+        }
+
+        [Test]
+        public void TestWorldUpdate_02_ThoroughlyOk()
+        {
+            // Arrange
+            const float TimeElapsed = 0.800000012f;
+            Vector seekingEntityPosition = new Vector(35, 35);
+            Vector targetEntityPosition = new Vector(200, 200);
+            WorldTest world = new WorldTest(250, 250, seekingEntityPosition, targetEntityPosition);
+            ISteeringBehavior steeringBehavior = new IdlingBehavior(world.SeekingEntity);
+            IMovingEntity movingEntity = world.SeekingEntity;
+            movingEntity.SetSteeringBehavior(steeringBehavior);
+
+            // Act & assert
+            Assert.AreEqual("(35,35)", movingEntity.Position.ToString());
+            Assert.AreEqual("(0,0)", movingEntity.Velocity.ToString());
+            BehaviorTestUtil.AssertMovingEntityWithSeekingBehavior(
+                movingEntity,
+                steeringBehavior,
+                TimeElapsed,
+                "(35,35)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(35,35)"
+            );
+
+            for (int index = 0; index < 100; index++)
+            {
+                world.Update(TimeElapsed);
+            }
+
+            BehaviorTestUtil.AssertMovingEntityWithSeekingBehavior(
+                movingEntity,
+                steeringBehavior,
+                TimeElapsed,
+                "(35,35)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(0,0)",
+                "(35,35)"
+            );
+            Assert.AreEqual("(35,35)", movingEntity.Position.ToString());
+            Assert.AreEqual("(0,0)", movingEntity.Velocity.ToString());
         }
     }
 }
