223c3c1d16847ba6ca369c92a4e7a08bfd117031
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-03-16 10:46:27 +0100

    Add tests for hitbox

--- Src/util/HitBox.cs
+++ Src/util/HitBox.cs
@@ -6,10 +6,57 @@ namespace Src.util
     {
         private readonly IGameEntity _entity;
 
-        public Vector UpperLeftCorner => _entity.Position + new Vector(-_entity.Width / 2, -_entity.Height / 2);
-        public Vector UpperRightCorner => _entity.Position + new Vector(_entity.Width / 2, -_entity.Height / 2);
-        public Vector LowerLeftCorner => _entity.Position + new Vector(-_entity.Width / 2, _entity.Height / 2);
-        public Vector LowerRightCorner => _entity.Position + new Vector(_entity.Width / 2, _entity.Height / 2);
+        public Vector UpperLeftCorner
+        {
+            get
+            {
+                if (_entity == null)
+                {
+                    return null;
+                }
+
+                return _entity.Position + new Vector(-_entity.Width / 2, -_entity.Height / 2);
+            }
+        }
+
+        public Vector UpperRightCorner
+        {
+            get
+            {
+                if (_entity == null)
+                {
+                    return null;
+                }
+                
+                return _entity.Position + new Vector(_entity.Width / 2, -_entity.Height / 2);
+            }
+        }
+
+        public Vector LowerLeftCorner
+        {
+            get
+            {
+                if (_entity == null)
+                {
+                    return null;
+                }
+                
+                return _entity.Position + new Vector(-_entity.Width / 2, _entity.Height / 2);
+            }
+        }
+
+        public Vector LowerRightCorner
+        {
+            get
+            {
+                if (_entity == null)
+                {
+                    return null;
+                }
+                
+                return _entity.Position + new Vector(_entity.Width / 2, _entity.Height / 2);
+            }
+        }
 
         public HitBox(IGameEntity entity)
         {
