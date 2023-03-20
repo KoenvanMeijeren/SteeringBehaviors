06a9c318fbc69a811e2e281fb8eed976493aac3d
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-03-20 15:41:55 +0100

    Add position start and end property

--- Src/grid/GridTile.cs
+++ Src/grid/GridTile.cs
@@ -8,6 +8,8 @@ namespace Src.grid
         public readonly int SizeCenter;
         public readonly Vector Position;
         public readonly Vector PositionCenter;
+        public readonly Vector PositionStart;
+        public readonly Vector PositionEnd;
 
         protected GridTile(int size, int x, int y)
         {
@@ -15,6 +17,8 @@ namespace Src.grid
             SizeCenter = size / 2;
             Position = new Vector(x, y);
             PositionCenter = new Vector(x + SizeCenter, y + SizeCenter);
+            PositionStart = new Vector(x, y);
+            PositionEnd = new Vector(x + size, y + size);
         }
     }
 }
