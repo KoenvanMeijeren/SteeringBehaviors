e5911188deee42b9d192148ee2a44d283c3ca732
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-03-21 13:27:20 +0100

    Add detailed position structure properties to grid tile

--- Src/grid/GridTile.cs
+++ Src/grid/GridTile.cs
@@ -4,13 +4,17 @@ namespace Src.grid
 {
     public abstract class GridTile
     {
-        public readonly int Size;
-        public readonly Vector Position;
+        public readonly int Size, SizeCenter;
+        public readonly Vector Position, PositionCenter, PositionStart, PositionEnd;
 
         protected GridTile(int size, int x, int y)
         {
             Size = size;
+            SizeCenter = size / 2;
             Position = new Vector(x, y);
+            PositionCenter = new Vector(x + SizeCenter, y + SizeCenter);
+            PositionStart = new Vector(x, y);
+            PositionEnd = new Vector(x + size, y + size);
         }
     }
 }
