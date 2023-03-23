858cb265b2a331a5d80029006e59a630cea5a9da
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-03-23 10:17:23 +0100

    Add utility class for ahead positions generation

---
+++ Src/util/AheadPositionsUtil.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Src.util
+{
+    public static class AheadPositionsUtil
+    {
+        public static Tuple<Vector, Vector, Vector> Generate(Vector vector, bool isDirectionRight, bool isDirectionUpwards, bool isDirectionDownwards)
+        {
+            Vector vectorDiagonalUpper;
+            Vector vectorDiagonalLower;
+            if (isDirectionRight)
+            {
+                if (isDirectionDownwards)
+                {
+                    vectorDiagonalUpper = (new Vector(vector.X - 32, vector.Y + 32));
+                    vectorDiagonalLower = (new Vector(vector.X + 32, vector.Y + 32));
+
+                    return new Tuple<Vector, Vector, Vector>(vector, vectorDiagonalUpper, vectorDiagonalLower);
+                }
+                
+                vectorDiagonalUpper = (new Vector(vector.X + 32, vector.Y - 32));
+                vectorDiagonalLower = (new Vector(vector.X + 32, vector.Y + 32));
+
+                return new Tuple<Vector, Vector, Vector>(vector, vectorDiagonalUpper, vectorDiagonalLower);
+            }
+            
+            if (isDirectionDownwards)
+            {
+                vectorDiagonalUpper = (new Vector(vector.X + 32, vector.Y + 32));
+                vectorDiagonalLower = (new Vector(vector.X - 32, vector.Y + 32));
+
+                return new Tuple<Vector, Vector, Vector>(vector, vectorDiagonalUpper, vectorDiagonalLower);
+            }
+
+            if (isDirectionUpwards)
+            {
+                vectorDiagonalUpper = (new Vector(vector.X + 32, vector.Y - 32));
+                vectorDiagonalLower = (new Vector(vector.X - 32, vector.Y - 32));
+
+                return new Tuple<Vector, Vector, Vector>(vector, vectorDiagonalUpper, vectorDiagonalLower);
+            }
+
+            vectorDiagonalUpper = (new Vector(vector.X - 32, vector.Y - 32));
+            vectorDiagonalLower = (new Vector(vector.X - 32, vector.Y + 32));
+
+            return new Tuple<Vector, Vector, Vector>(vector, vectorDiagonalUpper, vectorDiagonalLower);
+        }
+    }
+}
---
+++ Tests/util/AheadPositionsUtilTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Src.util;
+
+namespace Tests.util
+{
+    public class AheadPositionsUtilTests
+    {
+        [TestCase(new double[]{150,100}, true, true, true, "((150,100), (118,132), (182,132))")]
+        [TestCase(new double[]{150,100}, true, true, false, "((150,100), (182,68), (182,132))")]
+        [TestCase(new double[]{150,100}, true, false, false, "((150,100), (182,68), (182,132))")]
+        [TestCase(new double[]{150,100}, true, false, true, "((150,100), (118,132), (182,132))")]
+        [TestCase(new double[]{150,100}, false, true, true, "((150,100), (182,132), (118,132))")]
+        [TestCase(new double[]{150,100}, false, true, false, "((150,100), (182,68), (118,68))")]
+        [TestCase(new double[]{150,100}, false, false, true, "((150,100), (182,132), (118,132))")]
+        [TestCase(new double[]{150,100}, false, false, false, "((150,100), (118,68), (118,132))")]
+        public void Generate_01_Ok(double[] positions, bool isDirectionRight, bool isDirectionUpwards, bool isDirectionDownwards, string expectedResult)
+        {
+            // Arrange
+            Vector vector = new Vector(positions[0], positions[1]);
+
+            // Act
+            Tuple<Vector, Vector, Vector> result = AheadPositionsUtil.Generate(vector, isDirectionRight, isDirectionUpwards, isDirectionDownwards);
+
+            // Assert
+            Assert.That(result.ToString(), Is.EqualTo(expectedResult));
+        }
+    }
+}
