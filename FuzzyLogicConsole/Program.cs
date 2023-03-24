8d8cf91778c8e0d7e4f7604309a189db9b2b041e
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-03-24 09:41:57 +0100

    Add fuzzy logic console demo app

---
+++ FuzzyLogicConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FuzzyLogicConsole
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
