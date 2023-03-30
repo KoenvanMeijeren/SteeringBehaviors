ac53e5cf8fb1e764e0f09f031e97c8b460d97e17
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-03-30 16:13:44 +0200

    Update desirable value display

--- FuzzyLogicConsole/Program.cs
+++ FuzzyLogicConsole/Program.cs
@@ -49,9 +49,9 @@ namespace FuzzyLogicConsole
 
                     string desirableValue = deFuzzifiedValue switch
                     {
-                        >= 0.40 and <= 0.75 => "desirable",
-                        >= 0.5 and <= 1.0 => "very desirable",
-                        _ => "undesirable"
+                        >= 0.40 and <= 0.75 => "desirable -> je hebt een beetje zin in eten en slaap ",
+                        >= 0.5 and <= 1.0 => "very desirable -> je hebt lekker gesmikkeld en mag gaan uitbuiken",
+                        _ => "undesirable -> je hebt heel snel veel eten en slaap nodig"
                     };
 
                     Console.WriteLine($"Desirability for hunger({hungerIndex:N2}) and sleep ({sleepIndex:N2}): " + deFuzzifiedValue.ToString("N2") + ": " + desirableValue);
