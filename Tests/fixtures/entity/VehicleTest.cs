a7a5461bd423dbbc4ff1ea3c3b6abe83864eecc8
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-03-31 14:18:54 +0200

    Debug vehicle test entity

--- Tests/fixtures/entity/VehicleTest.cs
+++ Tests/fixtures/entity/VehicleTest.cs
@@ -8,11 +8,13 @@ namespace Tests.fixtures.entity
     {
         public const int DefaultHeight = 10, DefaultWidth = 10;
         private const float DefaultMass = 30, DefaultMaxSpeed = 150;
+        public int Health { get; private set; } = 2;
         public VehicleTest(Vector position, IWorld world) : base(position, world, DefaultHeight, DefaultWidth)
         {
             Velocity = new Vector(0, 0);
             Mass = DefaultMass;
             MaxSpeed = DefaultMaxSpeed;
         }
+        public void TakeDamage() => Health--;
     }
 }
