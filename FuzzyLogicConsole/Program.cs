0531decb07072d4b3c6d3ce680560ac95d775432
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-04-07 09:34:12 +0200

    Update demo for fuzzy logic

--- FuzzyLogicConsole/Program.cs
+++ FuzzyLogicConsole/Program.cs
@@ -7,41 +7,45 @@ namespace FuzzyLogicConsole
 {
     internal static class Program
     {
+        private const string FuzzyVariableDistanceToFinishName = "DistanceToFinish", 
+            FuzzyVariableDistanceToNearestGoombaName = "DistanceToNearestGoomba",
+            FuzzyVariableDesirabilityName = "DesirabilityToFollowMario";
+        
         private static void Main()
         {
             Console.WriteLine("Fuzzy logic demo");
 
             FuzzyModule fuzzyModule = new();
 
-            FuzzyVariable hunger = fuzzyModule.CreateFlv("Hunger");
-            FuzzyTermSet starving = hunger.AddLeftShoulderSet("Starving", 0.0, 0.1, 0.3);
-            FuzzyTermSet content = hunger.AddTriangleSet("Content", 0.1, 0.5, 0.8);
-            FuzzyTermSet full = hunger.AddRightShoulderSet("Full", 0.5, 0.8, 1.0);
+            FuzzyVariable distanceToFinish = fuzzyModule.CreateFlv(FuzzyVariableDistanceToFinishName);
+            FuzzyTermSet distanceToFinishClose = distanceToFinish.AddLeftShoulderSet("Close", 0, 10, 30);
+            FuzzyTermSet distanceToFinishMedium = distanceToFinish.AddTriangleSet("Medium", 10, 50, 80);
+            FuzzyTermSet distanceToFinishFar = distanceToFinish.AddRightShoulderSet("Far", 50, 80, 100);
 
-            FuzzyVariable sleep = fuzzyModule.CreateFlv("Sleep");
-            FuzzyTermSet tired = sleep.AddLeftShoulderSet("Tired", 0.0, 0.1, 0.3);
-            FuzzyTermSet sleepy = sleep.AddTriangleSet("Sleepy", 0.1, 0.3, 0.5);
-            FuzzyTermSet awake = sleep.AddRightShoulderSet("Awake", 0.3, 0.5, 1.0);
+            FuzzyVariable distanceToNearestGoomba = fuzzyModule.CreateFlv(FuzzyVariableDistanceToNearestGoombaName);
+            FuzzyTermSet distanceToNearestGoombaClose = distanceToNearestGoomba.AddLeftShoulderSet("Close", 0, 10, 30);
+            FuzzyTermSet distanceToNearestGoombaMedium = distanceToNearestGoomba.AddTriangleSet("Medium", 10, 30, 50);
+            FuzzyTermSet distanceToNearestGoombaFar = distanceToNearestGoomba.AddRightShoulderSet("Far", 30, 50, 100);
 
-            FuzzyVariable desirability = fuzzyModule.CreateFlv("Desirability");
-            FuzzyTermSet undesirable = desirability.AddLeftShoulderSet("Undesirable", 0, 0.25, 0.5);
-            FuzzyTermSet desirable = desirability.AddTriangleSet("Desirable", 0.25, 0.5, 0.75);
-            FuzzyTermSet veryDesirable = desirability.AddRightShoulderSet("VeryDesirable", 0.5, 0.75, 1.0);
+            FuzzyVariable desirability = fuzzyModule.CreateFlv(FuzzyVariableDesirabilityName);
+            FuzzyTermSet undesirable = desirability.AddLeftShoulderSet("Undesirable", 0, 25, 50);
+            FuzzyTermSet desirable = desirability.AddTriangleSet("Desirable", 25, 50, 75);
+            FuzzyTermSet veryDesirable = desirability.AddRightShoulderSet("VeryDesirable", 50, 75, 100);
 
-            fuzzyModule.AddRule(new FuzzyOperatorOr(starving, tired), undesirable);
-            fuzzyModule.AddRule(new FuzzyOperatorAnd(content, sleepy), undesirable);
-            fuzzyModule.AddRule(new FuzzyOperatorAnd(full, sleepy), desirable);
-            fuzzyModule.AddRule(new FuzzyOperatorAnd(content, awake), desirable);
-            fuzzyModule.AddRule(new FuzzyOperatorAnd(full, awake), veryDesirable);
+            fuzzyModule.AddRule(new FuzzyOperatorOr(distanceToFinishClose, distanceToNearestGoombaClose), undesirable);
+            fuzzyModule.AddRule(new FuzzyOperatorAnd(distanceToFinishMedium, distanceToNearestGoombaMedium), undesirable);
+            fuzzyModule.AddRule(new FuzzyOperatorAnd(distanceToFinishFar, distanceToNearestGoombaMedium), desirable);
+            fuzzyModule.AddRule(new FuzzyOperatorAnd(distanceToFinishMedium, distanceToNearestGoombaFar), desirable);
+            fuzzyModule.AddRule(new FuzzyOperatorAnd(distanceToFinishFar, distanceToNearestGoombaFar), veryDesirable);
 
-            Console.WriteLine("Desirability for hunger and sleep if hidden : <= 0.5: undesirable");
-            for (double hungerIndex = 0; hungerIndex < 1; hungerIndex += 0.1)
+            Console.WriteLine("Desirability for DTF and DTNG if hidden : <= 50: undesirable");
+            for (double hungerIndex = 0; hungerIndex < 100; hungerIndex += 1)
             {
-                for (double sleepIndex = 0; sleepIndex < 1; sleepIndex += 0.1)
+                for (double sleepIndex = 0; sleepIndex < 100; sleepIndex += 1)
                 {
-                    fuzzyModule.Fuzzify("Hunger", hungerIndex);
-                    fuzzyModule.Fuzzify("Sleep", sleepIndex);
-                    double deFuzzifiedValue = fuzzyModule.DeFuzzify("Desirability");
+                    fuzzyModule.Fuzzify(FuzzyVariableDistanceToFinishName, hungerIndex);
+                    fuzzyModule.Fuzzify(FuzzyVariableDistanceToNearestGoombaName, sleepIndex);
+                    double deFuzzifiedValue = fuzzyModule.DeFuzzify(FuzzyVariableDesirabilityName);
                     if (deFuzzifiedValue == 0)
                     {
                         continue;
@@ -49,9 +53,9 @@ namespace FuzzyLogicConsole
 
                     string desirableValue = deFuzzifiedValue switch
                     {
-                        >= 0.40 and <= 0.75 => "desirable -> je hebt een beetje zin in eten en slaap ",
-                        >= 0.5 and <= 1.0 => "very desirable -> je hebt lekker gesmikkeld en mag gaan uitbuiken",
-                        _ => "undesirable -> je hebt heel snel veel eten en slaap nodig"
+                        >= 40 and <= 75 => "desirable -> to follow mario",
+                        >= 50 and <= 100 => "very desirable -> to follow mario",
+                        _ => "undesirable -> to follow mario"
                     };
 
                     Console.WriteLine($"Desirability for hunger({hungerIndex:N2}) and sleep ({sleepIndex:N2}): " + deFuzzifiedValue.ToString("N2") + ": " + desirableValue);
